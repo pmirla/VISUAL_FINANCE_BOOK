ff7bf902f3b5e10f8ca52f6602d291ba6ba26331
Pavan Mirla <pavanmirla@gmail.com>
2021-01-03 23:52:47 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  useHistory,
+  useParams
 } from "react-router-dom";
 import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
 
@@ -58,7 +60,7 @@ function App() {
                     <Component />
                   </Route>
                 ))}
-
+                <Route path="/galley" children={<Modal />} />
                 <Redirect path="/" to="/home/" />
               </Switch>
             </div>
@@ -66,6 +68,70 @@ function App() {
         </Router>
       </div>
     </ThemeProvider>
+  );
+}
+
+function Modal() {
+  let history = useHistory();
+  let { id } = useParams();
+  const IMAGES = [
+    { id: 0, title: "Dark Orchid", color: "DarkOrchid" },
+    { id: 1, title: "Lime Green", color: "LimeGreen" },
+    { id: 2, title: "Tomato", color: "Tomato" },
+    { id: 3, title: "Seven Ate Nine", color: "#789" },
+    { id: 4, title: "Crimson", color: "Crimson" }
+  ];
+  let image = IMAGES[parseInt(id, 10)];
+
+  if (!image) return null;
+
+  let back = (e) => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
+  return (
+    <div
+      onClick={back}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: "rgba(0, 0, 0, 0.15)"
+      }}
+    >
+      <div
+        className="modal"
+        style={{
+          position: "absolute",
+          background: "#fff",
+          top: 25,
+          left: "10%",
+          right: "10%",
+          padding: 15,
+          border: "2px solid #444"
+        }}
+      >
+        <h1>{image.title}</h1>
+        <Image color={image.color} />
+        <button type="button" onClick={back}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+function Image({ color }) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: 400,
+        background: color
+      }}
+    />
   );
 }
 
